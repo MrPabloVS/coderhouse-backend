e66dbd33b680e3be6248574bc141ad06e88c9bfa
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-06-23 15:45:47 -0300

    heroku try 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const Dotenv = require('dotenv-webpack');
 module.exports = {
-    plugins: [
-        new Dotenv()
-    ]
-}
+    resolve: {
+      
+      fallback: {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+      } 
+    }}
